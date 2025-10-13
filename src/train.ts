5749a71e7960e14114f319d0e8e95ea3b9ab09c0
Nick <javokhirru03@gmail.com>
2025-10-13 17:17:14 +0900

    feat: express integration & mongoose + tack H

--- src/train.ts
+++ src/train.ts
@@ -1,15 +1,23 @@
-function getHighestIndex(arr: number[]): number {
-  let max: number = arr[0];
-  let index: number = 0;
+// function getHighestIndex(arr: number[]): number {
+//   let max: number = arr[0];
+//   let index: number = 0;
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-      index = i;
-    }
-  }
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//       index = i;
+//     }
+//   }
 
-  return index;
+//   return index;
+// }
+
+// console.log(getHighestIndex([5, 21, 12, 21, 8]));
+
+//..................................................Tack - H ..............................//
+function getPositive(arr: number[]): string {
+  return arr.filter((num) => num > 0).join("");
 }
 
-console.log(getHighestIndex([5, 21, 12, 21, 8]));
+console.log(getPositive([1, -4, 2]));
+console.log(typeof getPositive([1, -4, 2]));
